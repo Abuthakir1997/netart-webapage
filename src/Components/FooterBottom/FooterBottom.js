0f851d9935dd5a1778f3b0c03ed988703b2d1ef2
abumj <50373170+abumj@users.noreply.github.com>
2020-09-12 12:52:09 +0530

    bugs

--- src/Components/FooterBottom/FooterBottom.js
+++ src/Components/FooterBottom/FooterBottom.js
@@ -8,17 +8,17 @@ const footerbottom = () => {
             <div className="row m-0 p-0">
                 <div className="col-xl-4 col-md-12">
                     <ul>
-                        <li><i class="icofont-phone-circle"></i><a href="#">Tollfree-1800 200 1234</a></li>
+                        <li><i class="icofont-phone-circle"></i><a href="http://www.google.com/">Tollfree-1800 200 1234</a></li>
                     </ul>
                 </div>
                 <div className="col-xl-4 col-md-12">
                     <ul>
-                        <li><i class="icofont-facebook"></i><a href="#">www.facebook.com/cripumps</a></li>
+                        <li><i class="icofont-facebook"></i><a href="http://www.google.com/">www.facebook.com/cripumps</a></li>
                     </ul>
                 </div>
                 <div className="col-xl-4 col-md-12">
                     <ul>
-                        <li><i class="icofont-world"></i><a href="#">www.crigroups.com</a></li>
+                        <li><i class="icofont-world"></i><a href="http://www.google.com/">www.crigroups.com</a></li>
                     </ul>
                 </div>
 
