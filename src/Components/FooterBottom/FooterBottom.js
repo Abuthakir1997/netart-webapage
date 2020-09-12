a90b53fedbc0e140a445d96a2672518c415012b6
abumj <50373170+abumj@users.noreply.github.com>
2020-09-12 14:13:10 +0530

    changes

--- src/Components/FooterBottom/FooterBottom.js
+++ src/Components/FooterBottom/FooterBottom.js
@@ -2,23 +2,23 @@ import React from 'react';
 import Radium from 'radium';
 import '../FooterBottom/FooterBottom.css';
 
-const footerbottom = () => {
+const footerBottom = () => {
     return (
         <div className="footer-bottom">
             <div className="row m-0 p-0">
                 <div className="col-xl-4 col-md-12">
                     <ul>
-                        <li><i class="icofont-phone-circle"></i><a href="http://www.google.com/">Tollfree-1800 200 1234</a></li>
+                        <li><i class="icofont-phone-circle"></i><a href="/http://www.google.com" target="_blank">Tollfree-1800 200 1234</a></li>
                     </ul>
                 </div>
                 <div className="col-xl-4 col-md-12">
                     <ul>
-                        <li><i class="icofont-facebook"></i><a href="http://www.google.com/">www.facebook.com/cripumps</a></li>
+                        <li><i class="icofont-facebook"></i><a href="http://www.facebook.com/cripumps" target="_blank">www.facebook.com/cripumps</a></li>
                     </ul>
                 </div>
                 <div className="col-xl-4 col-md-12">
                     <ul>
-                        <li><i class="icofont-world"></i><a href="http://www.google.com/">www.crigroups.com</a></li>
+                        <li><i class="icofont-world"></i><a href="http://www.crigroups.com/" target="_blank">www.crigroups.com</a></li>
                     </ul>
                 </div>
 
@@ -29,4 +29,4 @@ const footerbottom = () => {
     );
 }
 
-export default Radium(footerbottom);
+export default Radium(footerBottom);
